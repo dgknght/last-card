5a357f816d4ebafd4ed66c52bb19ede966035290
Elijah Knight <eli.knight324@gmail.com>
2020-07-29 11:42:10 -0500

    created game + game_test files as well as game.start functions and tests

---
+++ models/game.js
@@ -0,0 +1,31 @@
+const Deck = require('./deck');
+
+const Game = function(players) {
+
+  this._players = players;
+  this._deck = new Deck();
+  this._discardPile = [];
+
+  this.start = function() {
+    this._deck.shuffle();
+    for (let step = 0; step < 7; step++) {
+      this._players.forEach(p => p.drawCard(this._deck.deal()));
+    };
+    this._discardPile.push(this._deck.deal());
+  };
+
+  this.getPlayers = function() {
+    return this._players;
+  };
+
+  this.getDiscardPile = function() {
+    return this._discardPile;
+  };
+
+  this.getDeck = function() {
+    return this._deck;
+  };
+
+};
+
+module.exports = Game;
---
+++ test/game_test.js
@@ -0,0 +1,28 @@
+const expect = require('chai').expect;
+const Game = require('../models/game');
+const Player = require('../models/player');
+
+describe('Game', () => {
+  describe('.start()', () => {
+    const player1 = new Player();
+    const player2 = new Player();
+    const game = new Game([player1, player2]);
+    game.start();
+
+    it('deals each player the correct amount of cards', () => {
+      expect(game.getPlayers().length).to.equal(2);
+      expect(game.getPlayers()[0].getCardCount()).to.equal(7);
+      expect(game.getPlayers()[1].getCardCount()).to.equal(7);
+    });
+
+    it('puts 1 card in discard pile', () => {
+      expect(game.getDiscardPile().length).to.equal(1);
+    });
+
+    it('leaves correct number of cards in deck', () => {
+      expect(game.getDeck().getCardCount()).to.equal(93); // 108 - (7 + 7 + 1) = 93
+    });
+
+
+  });
+});
