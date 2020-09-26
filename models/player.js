c4ef1855742251faa0ec9be1812996ba1dd89ca9
Elijah Knight <eli.knight324@gmail.com>
2020-09-26 13:52:47 -0500

    Added .getName, .removeCard returns bool

--- models/player.js
+++ models/player.js
@@ -1,17 +1,27 @@
-const Player = function() {
+const Player = function(name) {
   this._hand = [];
+  this._name = name;
 
   this.drawCard = function(card) {
     this._hand.push(card);
   };
 
   this.removeCard = function(card) {
-    this._hand = this._hand.filter(c => !c.equals(card));
+    const index = this._hand.findIndex(c => c.equals(card));
+    if (index == -1) return false;
+    this._hand.splice(index, 1);
+    return true;
   };
 
   this.getCardCount = function() {
     return this._hand.length;
   };
+
+  this.getName = function() {
+    return this._name;
+  };
+
+  // this.getNameX = () => this._name;
 };
 
 module.exports = Player;
