ba0c7d761e5d532741b7a8131e5d091186337ca4
Elijah Knight <eli.knight324@gmail.com>
2020-09-26 13:53:59 -0500

    Fixed cross contamination error, added pending

--- test/game_test.js
+++ test/game_test.js
@@ -4,6 +4,9 @@ const Card = require('../models/card').Card;
 const Player = require('../models/player');
 
 describe('Game', () => {
+
+  it('does not allow duplicate names');
+  
   describe('.start()', () => {
     const player1 = new Player();
     const player2 = new Player();
@@ -30,41 +33,85 @@ describe('Game', () => {
   });
 
   describe('.playCard()', () => {
-    const player1 = new Player();
-    const player2 = new Player();
-    const deck = new TestDeck([
-      new Card(0, 'blue'),
-      new Card(1, 'blue'),
-      new Card(2, 'blue'),
-      new Card(3, 'blue'),
-      new Card(4, 'blue'),
-      new Card(5, 'blue'),
-      new Card(6, 'blue'),
-      new Card(7, 'blue'),
-      new Card(8, 'blue'),
-      new Card(9, 'blue'),
-      new Card(1, 'blue'),
-      new Card(2, 'blue'),
-      new Card(3, 'blue'),
-      new Card(4, 'blue'),
-      new Card(5, 'blue')
-    ]);
-    const game = new Game([player1, player2], deck);
-    game.start();
-    game.playCard(new Card(0, 'blue'));
+    describe('with a valid card', () => {
+      const player1 = new Player('player1');
+      const player2 = new Player('player2');
+      const deck = new TestDeck([
+        new Card(0, 'blue'),
+        new Card(1, 'blue'),
+        new Card(2, 'blue'),
+        new Card(3, 'blue'),
+        new Card(4, 'blue'),
+        new Card(5, 'blue'),
+        new Card(6, 'blue'),
+        new Card(7, 'blue'),
+        new Card(8, 'blue'),
+        new Card(9, 'blue'),
+        new Card(1, 'blue'),
+        new Card(2, 'blue'),
+        new Card(3, 'blue'),
+        new Card(4, 'blue'),
+        new Card(5, 'blue')
+      ]);
+      const game = new Game([player1, player2], deck);
+      game.start();
+      game.playCard(new Card(0, 'blue'));
 
-    it('puts selected card on the discard pile', () => {
-      expect(game.getTopDiscard().equals(new Card(0, 'blue'))).to.be.true;
+      it('puts selected card on the discard pile', () => {
+        expect(game.getTopDiscard().equals(new Card(0, 'blue'))).to.be.true;
+      });
+
+      it("removes selected card from the player's hand", () => {
+        expect(game.getPlayers()[0].getCardCount()).to.equal(6);
+      });
+
+      it('advances the current player', () => {
+        expect(game.getCurrentPlayer().getName()).to.equal(player2.getName());
+      });
     });
 
-    it("removes selected card from the player's hand", () => {
-      expect(game.getPlayers()[0].getCardCount()).to.equal(6);
+    describe('with an invalid card', () => {
+      const player1 = new Player('player1');
+      const player2 = new Player('player2');
+      const deck = new TestDeck([
+        new Card(0, 'blue'),
+        new Card(1, 'blue'),
+        new Card(2, 'blue'),
+        new Card(3, 'blue'),
+        new Card(4, 'blue'),
+        new Card(5, 'blue'),
+        new Card(6, 'blue'),
+        new Card(7, 'blue'),
+        new Card(8, 'blue'),
+        new Card(9, 'blue'),
+        new Card(1, 'blue'),
+        new Card(2, 'blue'),
+        new Card(3, 'blue'),
+        new Card(4, 'blue'),
+        new Card(5, 'blue')
+      ]);
+      const game = new Game([player1, player2], deck);
+      game.start();
+      var error = null;
+      try {
+        game.playCard(new Card(0, 'red'));
+      } catch (e) {
+        error = e;
+      }
+
+      it('rejects an invalid card', () => {
+        expect(error).not.to.be.null;
+      });
+
+      it('does not put selected card on the discard pile', () => {
+        expect(game.getTopDiscard().equals(new Card(0, 'blue'))).to.be.false;
+      });
+
+      it('does not advance the current player', () => {
+        expect(game.getCurrentPlayer().getName()).to.equal(player1.getName());
+      });
     });
 
-    it('advances the current player', () => {
-      expect(game.getCurrentPlayer()).to.equal(player2);
-    });
-    // TODO: Test for invalid card
     // TODO: Test that the first player goes after the last player
   });
 });
