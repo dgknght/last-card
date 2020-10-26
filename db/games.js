babb880ba6b21c80524777cadaae7c39610e02b5
Doug Knight <dknight@getaroom.com>
2020-10-26 10:32:34 -0500

    remove more redundancy

--- db/games.js
+++ db/games.js
@@ -1,36 +1,39 @@
 const { MongoClient, ObjectID } = require('mongodb');
 
 module.exports = class Games {
-  async _mongo(f) {
+  // either _mongo('collection_name', f) or _mongo(f)
+  async _mongo(collection, f) {
+    if (f == null) {
+      f = collection;
+      collection = null;
+    }
     const client = new MongoClient(process.env.MONGODB_URL, { useUnifiedTopology: true });
     try {
       await client.connect();
       const database = client.db(process.env.MONGODB_DATABASE);
-      return await f(database);
+      const arg = collection == null ? database : database.collection(collection);
+      return await f(arg);
     } finally {
       client.close();
     }
   }
 
   async search() {
-    return this._mongo(db => {
-      const collection = db.collection('games');
-      const cursor = collection.find({}, { limit: 100, sort: { name: 1 } });
+    return this._mongo('games', col => {
+      const cursor = col.find({}, { limit: 100, sort: { name: 1 } });
       return cursor.toArray()
     });
   }
 
   async find(id) {
-    return this._mongo(db => {
-      const collection = db.collection('games');
-      return collection.findOne({ _id: ObjectID(id) });
+    return this._mongo('games', col => {
+      return col.findOne({ _id: ObjectID(id) });
     });
   }
 
   async create(game) {
-    return this._mongo(db => {
-      const collection = db.collection('games');
-      return collection.insertOne(game)
+    return this._mongo('games', col => {
+      return col.insertOne(game)
         .then(r => Promise.resolve(r.insertedId));
     });
   }
