01af9c8cbf99d8651dd718740345984b7f9e82f1
Doug Knight <dougbknight@gmail.com>
2020-10-25 21:36:28 -0500

    extract _mongo function

--- db/games.js
+++ db/games.js
@@ -1,41 +1,37 @@
 const { MongoClient, ObjectID } = require('mongodb');
 
 module.exports = class Games {
-  async search() {
+  async _mongo(f) {
     const client = new MongoClient(process.env.MONGODB_URL, { useUnifiedTopology: true });
     try {
       await client.connect();
       const database = client.db(process.env.MONGODB_DATABASE);
-      const collection = database.collection('games');
-      const cursor = collection.find({}, { limit: 100, sort: { name: 1 } });
-      return await cursor.toArray()
+      return await f(database);
     } finally {
       client.close();
     }
+  }
+
+  async search() {
+    return this._mongo(db => {
+      const collection = db.collection('games');
+      const cursor = collection.find({}, { limit: 100, sort: { name: 1 } });
+      return cursor.toArray()
+    });
   }
 
   async find(id) {
-    const client = new MongoClient(process.env.MONGODB_URL, { useUnifiedTopology: true });
-    try {
-      await client.connect();
-      const database = client.db(process.env.MONGODB_DATABASE);
-      const collection = database.collection('games');
-      return await collection.findOne({ _id: ObjectID(id) });
-    } finally {
-      client.close();
-    }
+    return this._mongo(db => {
+      const collection = db.collection('games');
+      return collection.findOne({ _id: ObjectID(id) });
+    });
   }
 
   async create(game) {
-    const client = new MongoClient(process.env.MONGODB_URL, { useUnifiedTopology: true });
-    try {
-      await client.connect();
-      const database = client.db(process.env.MONGODB_DATABASE);
-      const collection = database.collection('games');
-      const result = await collection.insertOne(game);
-      return result.insertedId;
-    } finally {
-      client.close();
-    }
+    return this._mongo(db => {
+      const collection = db.collection('games');
+      return collection.insertOne(game)
+        .then(r => Promise.resolve(r.insertedId));
+    });
   }
 };
